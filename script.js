5525df71b027fad27e3f96d96185fac7f578de5f
Amir Merchad <amir.merchad@tu-dortmund.de>
2025-07-05 01:29:15 +0200

    added basic js code

--- script.js
+++ script.js
@@ -1,3 +1,141 @@
+let a = 1.2;
+let b = 1.2;
+let currentOperator = "";
+let total = 1.2;
+const screenText = document.querySelector('.screenText');
+const calculation = document.querySelector('.calculation');
+
+function one(){
+    screenText.textContent = screenText.textContent + "1";
+}
+
+function two(){
+    screenText.textContent = screenText.textContent + "2";
+}
+
+function three(){
+    screenText.textContent = screenText.textContent + "3";
+}
+
+function four() {
+    screenText.textContent = screenText.textContent + "4";
+}
+
+function five() {
+    screenText.textContent = screenText.textContent + "5";
+}
+
+function six() {
+    screenText.textContent = screenText.textContent + "6";
+}
+
+function seven() {
+    screenText.textContent = screenText.textContent + "7";
+}
+
+function eight() {
+    screenText.textContent = screenText.textContent + "8";
+}
+
+function nine() {
+    screenText.textContent = screenText.textContent + "9";
+}
+
+function zero() {
+    screenText.textContent = screenText.textContent + "0";
+}
+
+function comma() {
+    screenText.textContent = screenText.textContent + ".";
+}
+
+function plus(){
+    if (screenText.textContent.includes("+") || screenText.textContent.includes("-") || screenText.textContent.includes("*") || screenText.textContent.includes("/")) {
+        calculate()
+    } else {
+        a = screenText.textContent;
+        screenText.textContent = screenText.textContent + "+"
+        currentOperator = "+"
+    }
+}
+
+function minus(){
+    if (screenText.textContent.includes("+") || screenText.textContent.includes("-") || screenText.textContent.includes("*") || screenText.textContent.includes("/")) {
+        calculate()
+    } else {
+        a = screenText.textContent;
+        screenText.textContent = screenText.textContent + "-"
+        currentOperator = "-"
+    }
+}
+
+function multiply(){
+    if (screenText.textContent.includes("+") || screenText.textContent.includes("-") || screenText.textContent.includes("*") || screenText.textContent.includes("/")) {
+        calculate()
+    } else {
+        a = screenText.textContent;
+        screenText.textContent = screenText.textContent + "*"
+        currentOperator = "*"
+    }
+}
+
+function divide(){
+    if (screenText.textContent.includes("+") || screenText.textContent.includes("-") || screenText.textContent.includes("*") || screenText.textContent.includes("/")) {
+        calculate()
+    } else {
+        a = screenText.textContent;
+        screenText.textContent = screenText.textContent + "/"
+        currentOperator = "/"
+    }
+}
+
+function Backspace() {
+    screenText.textContent = screenText.textContent.slice(0, -1);
+}
+
+function reset(){
+    a = 0
+    b = 0
+    calculation.textContent = ""
+    screenText.textContent = ""
+}
+
+function cleanNumber(value) {
+    const num = parseFloat(value);
+    return Number.isInteger(num) ? num : parseFloat(num.toFixed(2));
+}
+
+function calculate(){
+    if (currentOperator === "+") {
+        b = screenText.textContent.slice(screenText.textContent.indexOf("+") + 1);
+        a = parseFloat(a)
+        b = parseFloat(b)
+        total = a + b;
+        calculation.textContent = screenText.textContent;
+    } else if (currentOperator === "-") {
+        b = screenText.textContent.slice(screenText.textContent.indexOf("-") + 1);
+        a = parseFloat(a)
+        b = parseFloat(b)
+        total = a - b;
+        calculation.textContent = screenText.textContent;
+    } else if (currentOperator === "*") {
+        b = screenText.textContent.slice(screenText.textContent.indexOf("*") + 1);
+        a = parseFloat(a)
+        b = parseFloat(b)
+        total = a * b;
+        calculation.textContent = screenText.textContent;
+    } else if (currentOperator === "/") {
+        b = screenText.textContent.slice(screenText.textContent.indexOf("/") + 1);
+        a = parseFloat(a)
+        b = parseFloat(b)
+        total = a / b;
+        calculation.textContent = screenText.textContent;
+    }
+    total = cleanNumber(total);
+    screenText.textContent = total;
+}
+
+
 function setTheme(mode) {
     const header = document.querySelector('.header');
     const box = document.querySelector('.box');
